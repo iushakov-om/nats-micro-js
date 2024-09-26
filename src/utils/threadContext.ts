c3d527ae22898930dc6f3fbf7ca8b3f9509b02b3
Igor Ushakov <i.ushakov@optisoft.net>
2024-09-26 12:36:36 +0500

    Added ThreadContextStore type

--- src/utils/threadContext.ts
+++ src/utils/threadContext.ts
@@ -1,5 +1,7 @@
 import { AsyncLocalStorage } from 'async_hooks';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const threadContext = new AsyncLocalStorage<Map<string, any>>();
+export type ThreadContextStore = Map<string, any>
+
+export const threadContext = new AsyncLocalStorage<ThreadContextStore>();
 export const THREAD_CONTEXT_KEY_ADDITIONAL_HEADERS = 'additionalHeaders';
